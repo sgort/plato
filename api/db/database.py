52843e2a0b5ca7abc84a0b6bcfdd650c14eb5c03
Steven Gort <steven.gort@ictu.nl>
2026-04-15 19:35:39 +0200

    add logging

--- api/db/database.py
+++ api/db/database.py
@@ -1,9 +1,12 @@
+import logging
 from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 from config import settings
+
+logging.warning(f"DATABASE_URL = {settings.database_url}")
 
 engine = create_async_engine(settings.database_url, echo=False)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
